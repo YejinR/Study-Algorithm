9dd7ad335dc02f9eef034127817407b68bdf33c3
YejinR <117146772+YejinR@users.noreply.github.com>
2023-10-12 18:32:44 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.c
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.c
@@ -1,73 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char s1[1000], s2[1000], result[1000];
-    scanf("%s %s", s1, s2);
-    
-    int start1 = strlen(s1) - 1;
-    int start2 = strlen(s2) - 1;
-    for (int i = 0; i < strlen(s1); i++) {
-        if (s1[i] == '1') {
-            start1 = i;
-            break;
-        }
+int main(){
+  char A[81], B[81], result[81];
+  scanf("%s %s", A, B);
+
+  int startA = strlen(A)-1;
+  int startB = strlen(B)-1;
+
+  for(int i=0; i<strlen(A); i++){
+    if(A[i] == '1'){
+      startA = i;
+      break;
     }
-    for (int i = 0; i < strlen(s2); i++) {
-        if (s2[i] == '1') {
-            start2 = i;
-            break;
-        }
+  }
+  for(int i=0;i<strlen(B); i++){
+    if(B[i]=='1'){
+      startB = i;
+      break;
     }
-    
-    int end1 = strlen(s1) - 1;
-    int end2 = strlen(s2) - 1;
-    int carry = 0;
-    int resultIdx = 0;
-    while (1) {
-        int tmp = (s1[end1] - '0') + (s2[end2] - '0') + carry;
-        carry = tmp / 2;
-        result[resultIdx] = (tmp % 2) + '0';
-        
-        end1--;
-        end2--;
-        resultIdx++;
-        
-        if (end1 + 1 == start1)
-            break;
-        if (end2 + 1 == start2)
-            break;
+  }
+
+  int endA = strlen(A)-1;
+  int endB = strlen(B)-1;
+  int carry = 0 ;
+  int resultIndex = 0;
+
+  while(1){
+    int temp = (A[endA]-'0')+(B[endB]-'0')+carry;
+    carry = temp/2;
+    result[resultIndex] = (temp%2)+'0';
+
+    endA--;
+    endB--;
+    resultIndex++;
+
+    if(endA+1 == startA){
+      break;
     }
-    
-    for (; start1 != end1 + 1; end1--) {
-        int tmp = (s1[end1] - '0') + carry;
-        carry = tmp / 2;
-        result[resultIdx] = (tmp % 2) + '0';
-        resultIdx++;
+    if(endB+1 == startB){
+      break;
     }
-    for (; start2 != end2 + 1; end2--) {
-        int tmp = (s2[end2] - '0') + carry;
-        carry = tmp / 2;
-        result[resultIdx] = (tmp % 2) + '0';
-        resultIdx++;
-    }
-    
-    if (carry == 1) {
-        result[resultIdx] = carry + '0';
-        resultIdx++;
-    }
-    
-    result[resultIdx] = '\0';
-    
-    // 문자열 뒤집기
-    int len = strlen(result);
-    for (int i = 0; i < len / 2; i++) {
-        char temp = result[i];
-        result[i] = result[len - i - 1];
-        result[len - i - 1] = temp;
-    }
-    
-    printf("%s", result);
-    
-    return 0;
+  }
+
+  for(;startA != endA+1;endA--){ 
+    int temp = (A[endA]-'0')+carry;
+    carry = temp/2;
+    result[resultIndex] = (temp%2)+'0';
+    resultIndex++;
+  } //초기화 부분은 진행하지 않음, 그리고 for루프는 startA !=endA+1이 true일 동안 endA--를 하면서 돌아간다. 
+
+  for(;startB != endB+1;endB--){
+    int temp=(B[endB]-'0')+carry;
+    carry = temp/2;
+    result[resultIndex] = (temp%2)+'0';
+    resultIndex++;
+  }
+
+  if(carry == 1){
+    result[resultIndex] = carry + '0';
+    resultIndex++;
+  }
+
+  result[resultIndex] = '\0';
+
+  int len = strlen(result);
+  for(int i=0; i<len/2; i++){
+    char temp = result[i];
+    result[i]  =result[len-i-1];
+    result[len-i-1] = temp;
+  }
+
+  printf("%s", result);
+  return 0;
+
+  
 }
