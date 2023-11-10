6d7b80ecb18c7ce4c6a7848e762a3e3069084a25
YejinR <117146772+YejinR@users.noreply.github.com>
2023-11-10 14:10:17 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N, M, K;
+	scanf("%d %d %d", &N, &M, &K);
+
+	int max = (N / 2 < M) ? N / 2 : M;
+	
+	K -= N + M - 3 * max;
+
+	if (K > 0) {
+		K--;
+		max -= K / 3 + 1;
+	}
+
+	printf("%d\n", max);
+	return 0;
+}
