b89b7dffd23420736094bc46f036d7f05096b46e
YejinR <117146772+YejinR@users.noreply.github.com>
2023-12-07 23:17:05 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 4900 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.c
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -1,33 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-//#define min(a, b) a < b ? a : b 
-//다이나믹 프로그래밍(dp)
+#define MIN(a, b) a < b ? a : b
 
-
-int min(int a, int b) {
-	if (a < b) {
-		return a;
-	}
-	else {
-		return b;
-	}
-}
 
 int main() {
-	int dp[1000001] = { 0 };
-	int X;
-	scanf("%d", &X);
-	dp[1] = 0;
+	int N;
+	scanf("%d", &N);
+	int arr[1000001] = { 0 };
 
-	for(int i=2; i<=X; i++){
-		dp[i] = dp[i - 1] + 1;
+	arr[1] = 0;
+	for (int i = 2; i <= N; i++) {
+		arr[i] = arr[i - 1] + 1;
 		if (i % 3 == 0) {
-			dp[i] = min(dp[i], dp[i / 3] + 1); 
+			arr[i] = (MIN(arr[i], arr[i / 3] + 1));
 		}
 		if (i % 2 == 0) {
-			dp[i] = min(dp[i], dp[i / 2] + 1);
+			arr[i] = (MIN(arr[i], arr[i / 2] + 1));
 		}
 	}
-	printf("%d", dp[X]);
+	printf("%d", arr[N]);
 	return 0;
 }
